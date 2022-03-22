d105f6ba208b5eb9ac8bfe7d2815ef548cda3c68
amitmungare <amungare27@gmail.com>
2022-03-22 19:53:38 +0530

    index file

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
-// const env = require('./config/environment');
 const logger = require('morgan');
 
-
-// const cookieParser = require('cookie-parser');
 const app = express();
-// require('./config/view-helpers')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
@@ -17,37 +13,13 @@ const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
 const MongoStore = require('connect-mongo')(session);
-// const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
-// const customMware = require('./config/middleware');
-
-// // setup the chat server to be used with socket.io
-// const chatServer = require('http').Server(app);
-// const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-// chatServer.listen(5000);
-// console.log('chat server is listening on port 5000');
-// const path = require('path');
-
-
-// if (env.name == 'development'){
-//     app.use(sassMiddleware({
-//         src: path.join(__dirname, env.asset_path, 'scss'),
-//         dest: path.join(__dirname, env.asset_path, 'css'),
-//         debug: true,
-//         outputStyle: 'extended',
-//         prefix: '/css'
-//     }));
-// }
 
 app.use(express.urlencoded());
 
-// app.use(cookieParser());
-
-// app.use(express.static(env.asset_path));
 // make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-// app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
@@ -64,7 +36,6 @@ app.set('views', './views');
 // mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'chainkart',
-    // TODO change the secret before deployment in production mode
     secret: 'blahsomething',
     saveUninitialized: false,
     resave: false,
@@ -89,16 +60,12 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
 app.use(flash());
-// app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
 
 
 app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
-
+    if (err){console.log(`Error in running the server: ${err}`);}
     console.log(`Server is running on port: ${port}`);
 });
